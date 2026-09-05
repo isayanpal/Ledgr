a8c5cb7f8d0414dc5669b69b865f505e388cc013
Sayan Snigdha Pal <sayanpalb@gmail.com>
2026-09-05 23:16:45 +0530

    import fix

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { login } = require("../controlllers/auth");
+const { login, register } = require("../controllers/auth");
 const router = express.Router();
 
 router.post("/register", register);
